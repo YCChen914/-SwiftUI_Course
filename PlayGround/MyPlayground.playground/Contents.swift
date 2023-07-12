2608a8a3412b9ee64e8f9efbfad2d1222c055feb
PaulChen <csz890914@gmail.com>
2023-07-12 14:32:07 +0800

    Initial Commit

---
+++ PlayGround/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+//import UIKit
+
+var greeting = "Hello, playground"
+//print(greeting,"I am a Developer!")//逗點會有一個空格
+//print(greeting+"I am a Developer!")//串接 無空格
+//print("\(greeting)I am a Developer!")//用法 \()
+
+var person = "Limer"
+
+if person == "Limer"
+{
+    print("Hello , \(person)")
+}
+
+let pi = 3.1415926
+
+var circum = 2*5*pi
+print(circum)
